fd130127009e44f2467f6593779751542173ed96
Jonathan Matheus <jonathanmatheus@MacBook-de-Jonathan.local>
2019-09-25 01:19:12 -0300

    Updates

---
+++ precache-manifest.cc21b107f5eec43e78bcc6aa55393fbf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41caa423b97c3cf8ae398ab1063c57c",
+    "url": "/desafio/index.html"
+  },
+  {
+    "revision": "d9bd1bde76ede06971bc",
+    "url": "/desafio/static/css/2.2a1b566a.chunk.css"
+  },
+  {
+    "revision": "901985f7e849b406e30a",
+    "url": "/desafio/static/css/main.539f8d8e.chunk.css"
+  },
+  {
+    "revision": "d9bd1bde76ede06971bc",
+    "url": "/desafio/static/js/2.879376cd.chunk.js"
+  },
+  {
+    "revision": "901985f7e849b406e30a",
+    "url": "/desafio/static/js/main.55beab93.chunk.js"
+  },
+  {
+    "revision": "f98ce48141166d01a871",
+    "url": "/desafio/static/js/runtime~main.e69c3b5a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/desafio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/desafio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/desafio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/desafio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/desafio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "017704e7cd26a4437c75003d80d00ea8",
+    "url": "/desafio/static/media/logo-querobolsa.017704e7.svg"
+  }
+]);
